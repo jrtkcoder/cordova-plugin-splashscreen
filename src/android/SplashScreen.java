ebfa3b559c7e03be68ea1270165e34e27249742b
zmcoding <zmcoding@qq.com>
2019-02-23 14:42:13 +0800

    enable splash firsShow when resume the android app

--- src/android/SplashScreen.java
+++ src/android/SplashScreen.java
@@ -155,7 +155,7 @@ public class SplashScreen extends CordovaPlugin {
         // hide the splash screen to avoid leaking a window
         this.removeSplashScreen(true);
         // If we set this to true onDestroy, we lose track when we go from page to page!
-        //firstShow = true;
+        firstShow = true;
     }
 
     @Override
